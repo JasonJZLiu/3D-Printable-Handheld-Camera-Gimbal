312c3f8416aa2eb6de766dfd6e46bdba520475b0
JingzhouLiu <jingzhou.liu@ecf.utoronto.ca>
2020-09-07 12:52:19 -0400

    Code Uploaded

---
+++ Gimbal_Arduino_Code/Gimbal_Arduino_Code.ino
@@ -0,0 +1,299 @@
+#include <Wire.h>
+#include <Kalman.h> // Source: https://github.com/TKJElectronics/KalmanFilter
+#include <Servo.h>
+
+#define RESTRICT_PITCH
+
+Servo pitch3;
+Servo roll5;
+
+/*----------------------------PID - Roll----------------------------*/
+float roll_PID, pwm_L_F, pwm_L_B, pwm_R_F, pwm_R_B, roll_error, roll_previous_error;
+float roll_pid_p=0;
+float roll_pid_i=0;
+float roll_pid_d=0;
+
+double roll_kp=2;//3.55
+double roll_ki=0.1;//0.003
+double roll_kd=0.1;//2.05
+float roll_desired_angle = 0;    
+/*----------------------------PID - Roll----------------------------*/
+
+
+/*----------------------------PID - Pitch----------------------------*/
+float pitch_PID, pitch_error, pitch_previous_error;
+float pitch_pid_p=0;
+float pitch_pid_i=0;
+float pitch_pid_d=0;
+
+double pitch_kp=2;//3.55
+double pitch_ki=0.1;//0.003
+double pitch_kd=0.05;//2.05
+float pitch_desired_angle = 0;
+/*----------------------------PID - Pitch----------------------------*/
+
+float PWM_pitch = 90;
+float PWM_roll = 90;
+float elapsedTime, time, timePrev;
+
+/*----------------------------Input and Kalman Filter----------------------------*/
+Kalman kalmanX; 
+Kalman kalmanY;
+
+double accX, accY, accZ;
+double gyroX, gyroY, gyroZ;
+int16_t tempRaw;
+
+double gyroXangle, gyroYangle; // Angle calculate using the gyro only
+double compAngleX, compAngleY; // Calculated angle using a complementary filter
+double kalAngleX, kalAngleY; // Calculated angle using a Kalman filter
+
+const uint8_t IMUAddress = 0x68; // AD0 is logic low on the PCB
+const uint16_t I2C_TIMEOUT = 1000; // Used to check for errors in I2C communication
+
+uint32_t timer;
+uint8_t i2cData[14]; // Buffer for I2C data
+
+/*----------------------------Input and Kalman Filter----------------------------*/
+
+
+
+
+void setup() {
+  /*----------------------------Servo-Setup----------------------------*/
+  roll5.attach(5);
+  pitch3.attach(3);
+  /*----------------------------Servo-Setup----------------------------*/
+
+  Serial.begin(115200);
+  Wire.begin();
+#if ARDUINO >= 157
+  Wire.setClock(400000UL); // Set I2C frequency to 400kHz
+#else
+  TWBR = ((F_CPU / 400000UL) - 16) / 2; // Set I2C frequency to 400kHz
+#endif
+
+  i2cData[0] = 7; // Set the sample rate to 1000Hz - 8kHz/(7+1) = 1000Hz
+  i2cData[1] = 0x00; // Disable FSYNC and set 260 Hz Acc filtering, 256 Hz Gyro filtering, 8 KHz sampling
+  i2cData[2] = 0x00; // Set Gyro Full Scale Range to ±250deg/s
+  i2cData[3] = 0x00; // Set Accelerometer Full Scale Range to ±2g
+  while (i2cWrite(0x19, i2cData, 4, false)); // Write to all four registers at once
+  while (i2cWrite(0x6B, 0x01, true)); // PLL with X axis gyroscope reference and disable sleep mode
+
+  while (i2cRead(0x75, i2cData, 1));
+  if (i2cData[0] != 0x68) {
+    Serial.print(F("Error reading sensor"));
+    while (1);
+  }
+
+  delay(100); // Wait for sensor to stabilize
+
+  /* Set kalman and gyro starting angle */
+  while (i2cRead(0x3B, i2cData, 6));
+  accX = (int16_t)((i2cData[0] << 8) | i2cData[1]);
+  accY = (int16_t)((i2cData[2] << 8) | i2cData[3]);
+  accZ = (int16_t)((i2cData[4] << 8) | i2cData[5]);
+
+
+#ifdef RESTRICT_PITCH
+  double roll  = atan2(accY, accZ) * RAD_TO_DEG;
+  double pitch = atan(-accX / sqrt(accY * accY + accZ * accZ)) * RAD_TO_DEG;
+#else
+  double roll  = atan(accY / sqrt(accX * accX + accZ * accZ)) * RAD_TO_DEG;
+  double pitch = atan2(-accX, accZ) * RAD_TO_DEG;
+#endif
+
+  kalmanX.setAngle(roll); // Set starting angle
+  kalmanY.setAngle(pitch);
+  gyroXangle = roll;
+  gyroYangle = pitch;
+  compAngleX = roll;
+  compAngleY = pitch;
+
+  timer = micros();
+  time = millis();
+
+}
+
+void loop() {
+  timePrev = time;
+  time = millis();
+  elapsedTime = (time - timePrev) / 1000; 
+
+  /*----------------------------Input and Kalman Filter----------------------------*/
+  /* Update all the values */
+  while (i2cRead(0x3B, i2cData, 14));
+  accX = (int16_t)((i2cData[0] << 8) | i2cData[1]);
+  accY = (int16_t)((i2cData[2] << 8) | i2cData[3]);
+  accZ = (int16_t)((i2cData[4] << 8) | i2cData[5]);
+  tempRaw = (int16_t)((i2cData[6] << 8) | i2cData[7]);
+  gyroX = (int16_t)((i2cData[8] << 8) | i2cData[9]);
+  gyroY = (int16_t)((i2cData[10] << 8) | i2cData[11]);
+  gyroZ = (int16_t)((i2cData[12] << 8) | i2cData[13]);;
+
+  double dt = (double)(micros() - timer) / 1000000; // Calculate delta time
+  timer = micros();
+
+
+#ifdef RESTRICT_PITCH 
+  double roll  = atan2(accY, accZ) * RAD_TO_DEG;
+  double pitch = atan(-accX / sqrt(accY * accY + accZ * accZ)) * RAD_TO_DEG;
+#else // Eq. 28 and 29
+  double roll  = atan(accY / sqrt(accX * accX + accZ * accZ)) * RAD_TO_DEG;
+  double pitch = atan2(-accX, accZ) * RAD_TO_DEG;
+#endif
+
+  double gyroXrate = gyroX / 131.0; // Convert to deg/s
+  double gyroYrate = gyroY / 131.0; // Convert to deg/s
+
+#ifdef RESTRICT_PITCH
+  // This fixes the transition problem when the accelerometer angle jumps between -180 and 180 degrees
+  if ((roll < -90 && kalAngleX > 90) || (roll > 90 && kalAngleX < -90)) {
+    kalmanX.setAngle(roll);
+    compAngleX = roll;
+    kalAngleX = roll;
+    gyroXangle = roll;
+  } else
+    kalAngleX = kalmanX.getAngle(roll, gyroXrate, dt); // Calculate the angle using a Kalman filter
+
+  if (abs(kalAngleX) > 90)
+    gyroYrate = -gyroYrate; // Invert rate, so it fits the restriced accelerometer reading
+  kalAngleY = kalmanY.getAngle(pitch, gyroYrate, dt);
+#else
+  // This fixes the transition problem when the accelerometer angle jumps between -180 and 180 degrees
+  if ((pitch < -90 && kalAngleY > 90) || (pitch > 90 && kalAngleY < -90)) {
+    kalmanY.setAngle(pitch);
+    compAngleY = pitch;
+    kalAngleY = pitch;
+    gyroYangle = pitch;
+  } else
+    kalAngleY = kalmanY.getAngle(pitch, gyroYrate, dt); // Calculate the angle using a Kalman filter
+
+  if (abs(kalAngleY) > 90)
+    gyroXrate = -gyroXrate; // Invert rate, so it fits the restriced accelerometer reading
+  kalAngleX = kalmanX.getAngle(roll, gyroXrate, dt); // Calculate the angle using a Kalman filter
+#endif
+
+  gyroXangle += gyroXrate * dt; // Calculate gyro angle without any filter
+  gyroYangle += gyroYrate * dt;
+  //gyroXangle += kalmanX.getRate() * dt; // Calculate gyro angle using the unbiased rate
+  //gyroYangle += kalmanY.getRate() * dt;
+
+  compAngleX = 0.93 * (compAngleX + gyroXrate * dt) + 0.07 * roll; // Calculate the angle using a Complimentary filter
+  compAngleY = 0.93 * (compAngleY + gyroYrate * dt) + 0.07 * pitch;
+
+  // Reset the gyro angle when it has drifted too much
+  if (gyroXangle < -180 || gyroXangle > 180)
+    gyroXangle = kalAngleX;
+  if (gyroYangle < -180 || gyroYangle > 180)
+    gyroYangle = kalAngleY;
+
+  /* Print Data */
+  Serial.print(kalAngleX); Serial.print("   ");
+  Serial.print("\t");
+  Serial.print(kalAngleY); Serial.print("   ");
+  /*----------------------------Input and Kalman Filter----------------------------*/
+
+  
+  /*----------------------------PID----------------------------*/
+  roll_desired_angle = 2.4;   
+  pitch_desired_angle = 0;
+    
+  roll_error = kalAngleX - roll_desired_angle;
+  pitch_error = kalAngleY - pitch_desired_angle;    
+
+  roll_pid_p = roll_kp*roll_error;
+  pitch_pid_p = pitch_kp*pitch_error;
+
+
+  if (PWM_roll < 179 && PWM_roll > 0){
+    roll_pid_i = roll_pid_i+(roll_ki*roll_error);  
+  }else{
+    roll_pid_i = roll_pid_i;
+  }
+
+  if (PWM_pitch < 179 && PWM_pitch > 0){
+    pitch_pid_i = pitch_pid_i+(pitch_ki*pitch_error);  
+  }else{
+    pitch_pid_i = pitch_pid_i;
+  }
+
+  
+  roll_pid_d = roll_kd*((roll_error - roll_previous_error)/elapsedTime);
+  pitch_pid_d = pitch_kd*((pitch_error - pitch_previous_error)/elapsedTime);
+
+  roll_PID = roll_pid_p + roll_pid_i + roll_pid_d ;
+  pitch_PID = pitch_pid_p + pitch_pid_i + pitch_pid_d ;
+    
+  if(roll_PID < -90){roll_PID = -90;}
+  if(roll_PID > 90) {roll_PID = 90; }
+  if(pitch_PID < -90){pitch_PID = -90;}
+  if(pitch_PID > 90) {pitch_PID = 90;}
+    
+  roll_previous_error = roll_error;     
+  pitch_previous_error = pitch_error; 
+
+  PWM_pitch = 90 - pitch_PID;          
+  PWM_roll = 90 + roll_PID;
+    
+  pitch3.write(PWM_pitch);
+  roll5.write(PWM_roll);
+  /*----------------------------PID----------------------------*/
+
+  Serial.print("   |   ");
+  Serial.print("Roll PWM: ");
+  Serial.print(PWM_roll);
+  Serial.print("   |   ");
+  Serial.print("Pitch PWM: ");
+  Serial.println(PWM_pitch);
+  //delay(2);
+}
+
+
+
+
+/*----------------------------Functions----------------------------*/
+
+uint8_t i2cWrite(uint8_t registerAddress, uint8_t data, bool sendStop) {
+  return i2cWrite(registerAddress, &data, 1, sendStop); // Returns 0 on success
+}
+
+uint8_t i2cWrite(uint8_t registerAddress, uint8_t *data, uint8_t length, bool sendStop) {
+  Wire.beginTransmission(IMUAddress);
+  Wire.write(registerAddress);
+  Wire.write(data, length);
+  uint8_t rcode = Wire.endTransmission(sendStop); // Returns 0 on success
+  if (rcode) {
+    Serial.print(F("i2cWrite failed: "));
+    Serial.println(rcode);
+  }
+  return rcode; // See: http://arduino.cc/en/Reference/WireEndTransmission
+}
+
+uint8_t i2cRead(uint8_t registerAddress, uint8_t *data, uint8_t nbytes) {
+  uint32_t timeOutTimer;
+  Wire.beginTransmission(IMUAddress);
+  Wire.write(registerAddress);
+  uint8_t rcode = Wire.endTransmission(false); // Don't release the bus
+  if (rcode) {
+    Serial.print(F("i2cRead failed: "));
+    Serial.println(rcode);
+    return rcode; // See: http://arduino.cc/en/Reference/WireEndTransmission
+  }
+  Wire.requestFrom(IMUAddress, nbytes, (uint8_t)true); // Send a repeated start and then release the bus after reading
+  for (uint8_t i = 0; i < nbytes; i++) {
+    if (Wire.available())
+      data[i] = Wire.read();
+    else {
+      timeOutTimer = micros();
+      while (((micros() - timeOutTimer) < I2C_TIMEOUT) && !Wire.available());
+      if (Wire.available())
+        data[i] = Wire.read();
+      else {
+        Serial.println(F("i2cRead timeout"));
+        return 5; // This error value is not already taken by endTransmission
+      }
+    }
+  }
+  return 0; // Success
+}
